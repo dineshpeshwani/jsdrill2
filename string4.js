f8d2b6c0efa47b15f0bd6c4f4df37da6a30f4420
Dinesh Peshwani <dinesh.peshwani.20.1@mountblue.tech>
2022-06-08 10:12:45 +0530

    comments removed

--- string4.js
+++ string4.js
@@ -1,8 +1,6 @@
 function fullName(first_Name, last_Name)
 {
     let str = (first_Name.concat(" " ,last_Name));
-    //titleCase(name1);
-    //return result;
     str = str.toLowerCase().split(' ');
     for (var i = 0; i < str.length; i++) 
     {
@@ -11,14 +9,5 @@ function fullName(first_Name, last_Name)
     let result = str.join(' ');
     return result;
 }
-/*function titleCase(str) 
-{
-    str = str.toLowerCase().split(' ');
-    for (var i = 0; i < str.length; i++) 
-    {
-        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-    }
-    let result = str.join(' ');
-    return result;
-}*/
+
 module.exports = fullName;
